e2daa6a9f2d545605e7d3f2dfbe533393d7dd711
Ibrahim Said <ibrahimsaid3@cmail.carleton.ca>
2021-09-17 10:51:32 -0400

    Added new BuddyInfo and AddressBook objects to main

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -7,14 +7,29 @@ public class AddressBook {
         this.buddyCollection = new ArrayList<BuddyInfo>();
     }
 
+    public ArrayList<BuddyInfo> getBuddyCollection() {
+        return buddyCollection;
+    }
+
+    public void setBuddyCollection(ArrayList<BuddyInfo> buddyCollection) {
+        this.buddyCollection = buddyCollection;
+    }
+
     public void addBuddy(BuddyInfo buddy){
         buddyCollection.add(buddy);
     }
 
     public void removeBuddy(BuddyInfo buddy){
-        buddyCollection.remove(buddy);    }
+        buddyCollection.remove(buddy);
+    }
+
+
 
     public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Ibrahim", "123 Lala Land", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        System.out.println(addressBook.getBuddyCollection());
         System.out.println("Address Book.");
     }
 
