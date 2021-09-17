8167ba04dbe1d2f75071023ba1478800e9620129
Ibrahim Said <ibrahimsaid3@cmail.carleton.ca>
2021-09-17 10:42:42 -0400

    git commit

---
+++ src/AddressBook.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyCollection;
+
+    public AddressBook(){
+        this.buddyCollection = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        buddyCollection.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        buddyCollection.remove(buddy);    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+}
